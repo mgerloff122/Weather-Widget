ffd7e4ce2a62d0e5221c941781f942e9f42bdbaa
Matthew Gerloff <matthewgerloff@Hsin-Lings-MBP.fuel.digital>
2022-06-29 12:07:17 -0400

    completed widget

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '9aa580dfa2mshb1fbeb832e0c831p19595ajsn6e375d8d085a',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "149dba29546faca1a0da75950ca3e3d4";
